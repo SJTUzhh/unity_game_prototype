4e24ca41d9921a814973d8716c919a6a36be9f80
zhh <321984375@qq.com>
2021-12-16 22:02:09 +0800

    基本玩法

---
+++ 1_shipingguo/Assets/Scripts/AppleTree.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
+    void Start()
+    {
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        // 改变方向
+        if (pos.x < -leftAndRightEdge || pos.x > leftAndRightEdge)
+        {
+            speed *= -1;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
+}
---
+++ 1_shipingguo/Assets/Scripts/Basket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        transform.position = mousePos3D;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject collideWith = collision.gameObject;
+        if(collideWith.tag == "Apple")
+        {
+            Destroy(collideWith);
+        }
+    }
+}
